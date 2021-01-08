48c4c37a68e49585cf73c5ffee8f3c0d394d84d0
André Veiga <aa.veiga@campus.fct.unl.pt>
2021-01-08 17:53:16 +0000

    cleanup

--- BASE-0/Environment.java
+++ BASE-0/Environment.java
@@ -1,19 +1,11 @@
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Stack;
-
 import exceptions.DuplicateIdentifierException;
 import exceptions.UndeclaredIdentifierException;
 
-
 public class Environment<T> {
-
-	//static Stack<Environment> s;
 	HashMap<String, T> currentLevel;
-	
-	Environment<T> env;
+
 	Environment<T> prev;
-	ArrayList<Assoc<T>> assoc;
 
 	//push level
 	Environment<T>	beginScope() {
@@ -25,36 +17,14 @@ public class Environment<T> {
 		return prev;
 	}
 
-	void assoc(String	id,	T val) throws DuplicateIdentifierException{
-		for(Assoc<T> asso: assoc) {
-			if(asso.id.equals(id)) {
-				throw new DuplicateIdentifierException(id);
-			}
-		}
-		assoc.add(new Assoc<T>(id, val));
-		
-		 try {
-			if(currentLevel.containsKey(id))
-				throw new IDDeclaredTwiceException();
-			
-			currentLevel.put(id, val);
-		} catch(IDDeclaredTwiceException e) {
-			e.printStackTrace();
-		}
+	void assoc(String	id,	T val) throws DuplicateIdentifierException {
+		if(currentLevel.containsKey(id))
+			throw new DuplicateIdentifierException(id);
+
+		currentLevel.put(id, val);
 	}
 
 	public T find(String id) throws UndeclaredIdentifierException {
-		/*Environment<T> currentLevel = this;
-		while(currentLevel!=null) {
-			for(Assoc<T> assoc: currentLevel.assoc) {
-				if(assoc.id.equals(id)) {
-					return assoc.val;
-				}
-			}
-			currentLevel = currentLevel.env;
-		}
-		throw new UndeclaredIdentifierException(id);
-		*/
 		if(currentLevel.containsKey(id))
 			return currentLevel.get(id);
 		else if(prev == null)
@@ -64,24 +34,20 @@ public class Environment<T> {
 	}
 
 	public Environment() {
-		/*if(s == null)
-			s = new Stack<Environment>();*/
 		currentLevel = new HashMap<String,T>();
-		
-		//this.env = null;
+
 		this.prev = null;
-		//this.assoc = new ArrayList<Assoc<T>>();
 	}
-	
+
 	public Environment(Environment<T> env) {
 		this();
 		this.prev = env;
 	}
-	
+
 	static class Assoc<T>{
 		String id;
 		T val;
-		
+
 		Assoc(String id, T val){
 			this.id = id;
 			this.val = val;
